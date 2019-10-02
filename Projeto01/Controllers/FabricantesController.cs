47d895b81b25d13c7bd40a2de667e1e0ccac9a00
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2019-10-02 20:31:43 -0300

    Adaptação do Bootstrap - Edit, Details e Delete. Inclusão do Script no Index - Mensagem de remoção. TempData - POST (Delete)

--- Projeto01/Controllers/FabricantesController.cs
+++ Projeto01/Controllers/FabricantesController.cs
@@ -103,6 +103,7 @@ namespace Projeto01.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
