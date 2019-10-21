fafba81d8f7d45339c29abe3688ab0e3f3661994
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2019-10-21 17:07:10 -0300

    Camada Persistência

--- Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Controllers/CategoriasController.cs
@@ -1,11 +1,7 @@
 ﻿using Modelo.Cadastros;
-using Projeto01.Contexts;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Projeto01.Controllers
