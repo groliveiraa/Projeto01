3518f7ee6f0bbe523d09d6ad65c163836ffab689
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2019-12-19 21:34:56 -0300

    Identity e Classe Usuário

---
+++ Projeto01/DAL/IndentityDbContextAplicacao.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Projeto01.Areas.Seguranca.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projeto01.DAL
+{
+    public class IndentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+        {
+            public IdentityDbContextAplicacao() : base("IdentityDb") { }
+
+            static IdentityDbContextAplicacao()
+            {
+                Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+            }
+
+            public static IdentityDbContextAplicacao Create()
+            {
+                return new IdentityDbContextAplicacao();
+            }
+        }
+
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+        {
+        }
+    }
+}
